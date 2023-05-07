1dd0690ee28bd851c8e6cfd994d861c6efa096ac
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-05-07 00:57:30 -0400

    add: spring boot project with dependencies

---
+++ src/main/kotlin/ucb/judge/ujsubmissions/UjSubmissionsApplication.kt
@@ -0,0 +1,11 @@
+package ucb.judge.ujsubmissions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UjSubmissionsApplication
+
+fun main(args: Array<String>) {
+	runApplication<UjSubmissionsApplication>(*args)
+}
---
+++ src/test/kotlin/ucb/judge/ujsubmissions/UjSubmissionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ucb.judge.ujsubmissions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UjSubmissionsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
